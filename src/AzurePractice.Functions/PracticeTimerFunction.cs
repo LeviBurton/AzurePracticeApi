1af3c7b90dfb4414570d90730c1ba1992258d8a5
Levi Burton <levi.burton@gmail.com>
2026-08-17 15:29:56 -0500

    Add Azure Functions project with timer trigger

---
+++ src/AzurePractice.Functions/PracticeTimerFunction.cs
@@ -0,0 +1,30 @@
+using AzurePractice.Application;
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Extensions.Logging;
+
+namespace AzurePractice.Functions;
+
+public class PracticeTimerFunction
+{
+    private readonly ILogger<PracticeTimerFunction> _logger;
+    private readonly ICustomerService _customerService;
+
+    public PracticeTimerFunction(
+        ILogger<PracticeTimerFunction> logger,
+        ICustomerService customerService)
+    {
+        _logger = logger;
+        _customerService = customerService;
+    }
+
+    [Function("PracticeTimerFunction")]
+    public async Task Run(
+        [TimerTrigger("*/10 * * * * *")] TimerInfo myTimer)
+    {
+        var customers = await _customerService.GetAllAsync();
+
+        _logger.LogInformation(
+            "Timer executed. Customer count: {customerCount}",
+            customers.Count);
+    }
+}
